cc42eab22050a55c5ab080072052bc9450825318
ElkoDuka <eliqohen@gmail.com>
2025-05-29 23:15:08 +0700

    Create DataListSetTuple.py

---
+++ DataListSetTuple.py
@@ -0,0 +1,22 @@
+data_list = [1, 2, 2, 3, 4, 4, 5]
+print("Data List sebelum konversi:", data_list)
+data_set_from_list = set(data_list)
+print("Set hasil konversi dari List:", data_set_from_list)
+print()
+
+data_set = {10, 20, 30, 40}
+print("Data Set sebelum konversi:", data_set)
+data_list_from_set = list(data_set)
+print("List hasil konversi dari Set:", data_list_from_set)
+print()
+
+data_tuple = (100, 200, 200, 300)
+print("Data Tuple sebelum konversi:", data_tuple)
+data_set_from_tuple = set(data_tuple)
+print("Set hasil konversi dari Tuple:", data_set_from_tuple)
+print()
+
+data_set2 = {7, 8, 9, 10}
+print("Data Set sebelum konversi:", data_set2)
+data_tuple_from_set = tuple(data_set2)
+print("Tuple hasil konversi dari Set:", data_tuple_from_set)
